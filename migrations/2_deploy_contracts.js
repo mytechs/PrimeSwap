4d07bfea6afa9721b20e8c44342579b0f0cc9bd0
Kriyo <47378715+mytechs@users.noreply.github.com>
2022-05-20 12:31:59 +0530

    Token Deployment

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const Token = artifacts.require("Token");
+const PrimeSwap = artifacts.require("PrimeSwap");
+
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  // Deploy PrimeSwap
+  await deployer.deploy(PrimeSwap);
+  const primeSwap = await PrimeSwap.deployed()
+
+  // Transfer all tokens to PrimeSwap
+  await token.transfer(primeSwap.address, '1000000000000000000000000')
+};
